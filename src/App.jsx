a95d5365f2990c4d84581e4199b5bbdd703c45c3
upriser3011 <upriser3011@gmail.com>
2020-10-16 22:19:33 +0530

    Friday Night

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { hot } from 'react-hot-loader/root';
 import HelloWorld from './components/HelloWorld/HelloWorld';
 
 const App = () => {
-  return <HelloWorld title="Hello from React Webpack" />;
+  return <HelloWorld title="Kuldeep Singh from React Webpack" />;
 };
 
 export default hot(App);
