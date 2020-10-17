edc2c9f2ccbce5d280d650b410623ba749272c36
[Yash Pal] <[yashpalnss@gmail.com]>
2020-10-17 18:44:57 +0530

    first compo

---
+++ src/components/TestCompo/testCompo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const testCompo = prop => {
+  return (
+    <div>
+      <h1>This is my first component {prop.name}</h1>
+    </div>
+  );
+  //   return <h1>This is my first component.ff {props.name}</h1>;
+};
+
+export default testCompo;
