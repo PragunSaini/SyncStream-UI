cd4017abbaea49c9df289a0d6ea9f91b4e61424a
Pragun Saini <pragunsaini117@gmail.com>
2020-11-06 00:28:31 +0530

    added prod url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
+// const API_URL = 'http://localhost:8000'; // for dev
+const API_URL = 'http://52.230.105.58'; // for prod
 
 const getHeaders = () => {
   const userData = JSON.parse(window.localStorage.getItem('user')) || null;
